ddec89b7e7d3acccf786af5ffa8feb507c1c7404
nayy18228-ui <nayy18228@gmail.com>
2026-06-25 16:24:14 +0700

    Nameage.java

---
+++ Nameage.java
@@ -0,0 +1,20 @@
+void main(){
+
+
+    //IO.println("Enter your name ");
+   
+   // IO.println("Enter your age ");
+      IO.print(" Enter Your name: ");
+      String name = IO. readln();//ใช้กับตัวอักษร
+  
+      IO.print(" Enter Your age: ");
+      int age = Integer.parseInt(IO.readln());//ใช้กับตัวเลข
+
+      IO.println(" Hello "+ name + "ํ Your age is "+ age);
+
+
+
+
+
+
+}
